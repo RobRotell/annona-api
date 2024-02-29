7656bcb0671954b4efd62a53c8029963892a0d54
Rob <dev@robrotell.com>
2024-02-28 22:55:45 -0500

    Building out getItems route

--- src/routes/getItems.js
+++ src/routes/getItems.js
@@ -1,7 +1,33 @@
+import { Grocer } from '../controllers/Grocer.js'
+import { TimeKeeper } from '../controllers/Timekeeper.js'
+
+
 export const getItems = {
 	method: 'GET',
 	path: '/get',
+	options: {
+		auth: 'simple',
+	},
 	async handler( req, h ) {
+		const rawItems = await Grocer.getItems()
 
+		// Hapi likes a nice, clean array of objects
+		const items = Array.from( rawItems, ( [ key, value ] ) => {
+			return {
+				id: key,
+				name: value,
+			}
+		})
+
+		// report when items were last updated
+		const lastModified = await TimeKeeper.getLastModifiedDate()
+
+		return {
+			meta: {
+				count: items.length,
+				lastModified,
+			},
+			items
+		}
 	}
 }
