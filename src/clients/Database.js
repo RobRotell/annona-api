a950c84feccb8ff082e83bdc28a008acc3dd6a42
Rob <dev@robrotell.com>
2024-02-29 23:47:06 -0500

    Just making this a simple const now

--- src/clients/Database.js
+++ src/clients/Database.js
@@ -1,36 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
 
-export class Database {
-
-
-	static client = null
-
-
-	/**
-	 * Get client instance
-	 *
-	 * @return {object}
-	 */
-	static getClient() {
-		if ( !this.client ) {
-			this.#createClient()
-		}
-
-		return this.client
-	}
-
-
-	/**
-	 * Establish Prisma client
-	 *
-	 * @return {void}
-	 */
-	static #createClient() {
-		if ( null === this.client ) {
-			this.client = new PrismaClient()
-		}
-	}
-
-
-}
+// todo -- rename file
+export const prisma = new PrismaClient()
